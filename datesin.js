3dd7b6a02969b843423e98a060c9a33fd286a359
Cyberider1008 <abaranwal.it@gmail.com>
2025-03-04 22:08:49 +0530

    Updated JS files and removed test1.js

--- datesin.js
+++ datesin.js
@@ -1,27 +1,38 @@
-// //Dates
+//Dates
 
-// let date = new Date()
+let date = new Date()
 
-// console.log(date);
-// console.log(date.toDateString());
-// console.log(date.toString());
-// console.log(typeof date);
+console.log(date);
+console.log(date.toDateString());
+console.log(date.toString());
+console.log(typeof date);
 
 
 
-// let mycreateddate = new Date("11-19-2024")
-// console.log(mycreateddate.toLocaleString());
+let mycreateddate = new Date("11-19-2024")
+console.log(mycreateddate.toLocaleString());
 
-// let myTimeStamp = Date.now()
-// console.log(myTimeStamp)
-// console.log(mycreateddate.getTime());
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
+console.log(mycreateddate.getTime());
 
-// let newdate = new Date()
-
-// console.log(newdate);
-// console.log(newdate.getMonth()+1);
-// console.log(newdate.getDay());
-
-let newdate = newdate.toLocaleString('Default',{weekday:"long"})
+let newdate = new Date()
 
 console.log(newdate);
+console.log(newdate.getMonth()+1);
+console.log(newdate.getDay());
+
+//output
+// 2025-03-02T16:19:08.183Z
+// Sun Mar 02 2025
+// Sun Mar 02 2025 21:49:08 GMT+0530 (India Standard Time)
+// object
+// 11/19/2024, 12:00:00 AM
+// 1740932348309
+// 1731954600000
+// 2025-03-02T16:19:08.310Z
+// 3
+
+// let newdate = newdate.toLocaleString('Default',{weekday:"long"})
+
+// console.log(newdate);
--- num_math.js
+++ num_math.js
@@ -1,16 +1,16 @@
 const num = 100
-console.log(num);
+console.log(num); //100
 
 const num1 = new Number(100)
-console.log(num1);
-console.log(num1.toFixed(2));
-console.log(num1.toString());
-console.log(num1.toPrecision(2));
-console.log(num1.toLocaleString())
+console.log(num1); //[Number: 100]
+console.log(num1.toFixed(2)); //100.00
+console.log(num1.toString()); //100
+console.log(num1.toPrecision(2)); // 1.0e+2
+console.log(num1.toLocaleString()) //100
 ;
 
 // ++++++++++++++++++MATH++++++++++++++++++++
-console.log(Math.abs(-4));
+console.log(Math.abs(-4)); //4
 
 
 
